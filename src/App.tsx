8245037ddcef189d69587ddbff2b576a72e2198d
sangraal123 <googolheroicquests@gmail.com>
2024-05-31 05:59:02 +0900

    NFT Syncs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 // import { ConnectKitButton } from 'connectkit'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import {
   useAccount,
   // WindowProvider
-} from 'wagmi'
+} from "wagmi";
 
-import { Account } from './components'
+import { Account } from "./components";
 
 import {
   createWalletClient,
@@ -15,14 +15,14 @@ import {
   WalletClient,
   parseUnits,
   getAddress,
-} from 'viem'
-import { sepolia } from 'viem/chains'
-import { TokenboundClient } from '@tokenbound/sdk'
+} from "viem";
+import { sepolia } from "viem/chains";
+import { TokenboundClient } from "@tokenbound/sdk";
 
-import { useCallback, useEffect, useState } from 'react'
-import { Button } from '@mui/material';
+import { useCallback, useEffect, useState } from "react";
+import { Button } from "@mui/material";
 
-import Web3Mint from '../artifacts/contracts/Web3Mint.sol/Web3Mint.json';
+import Web3Mint from "../artifacts/contracts/Web3Mint.sol/Web3Mint.json";
 
 // interface WindowProvider {
 //   request: (...args: any[]) => Promise<any>
@@ -85,12 +85,12 @@ interface OwnedNFTsResponse {
   blockHash: string;
 }
 
-import NftCard from './components/nftcard';
+import NftCard from "./components/nftcard";
 
 declare global {
   interface Window {
     // ethereum?: WindowProvider
-    ethereum?: any // CoinbaseWalletSDK also defines window.ethereum, so we have to work around that :(
+    ethereum?: any; // CoinbaseWalletSDK also defines window.ethereum, so we have to work around that :(
   }
 }
 
@@ -104,39 +104,41 @@ declare global {
 // const TOKEN_ID = '5'
 
 export function App() {
-  const { isConnected, address } = useAccount()
+  const { isConnected, address } = useAccount();
 
   const walletClient: WalletClient = createWalletClient({
     chain: sepolia,
     account: address,
     // transport: http(),
     transport: window.ethereum ? custom(window.ethereum) : http(),
-  })
+  });
 
   const publicClient = createPublicClient({
     chain: sepolia,
-    transport: http()
-  })
+    transport: http(),
+  });
 
   const tokenboundClient = new TokenboundClient({
     walletClient,
     chainId: sepolia.id,
     // implementationAddress: '0x2d25602551487c3f3354dd80d76d54383a243358',
-  })
+  });
 
-  const [tokenContractInput, setTokenContractInput] = useState('0x0000000000000000000000000000000000000000')
-  const [tokenIdInput, setTokenIdInput] = useState('0')
-  const [NFTs, setNFTs] = useState<NFT[] | null>(null);
+  const [tokenContractInput, setTokenContractInput] = useState(
+    "0x0000000000000000000000000000000000000000"
+  );
+  const [tokenIdInput, setTokenIdInput] = useState("0");
+  const [CurrentNFTs, setCurrentNFTs] = useState<NFT[] | undefined>([]);
 
   useEffect(() => {
     async function testTokenboundClass() {
-      if (!tokenboundClient) return
+      if (!tokenboundClient) return;
 
       const tokenboundAccount = tokenboundClient.getAccount({
         tokenContract: tokenContractInput as `0x${string}`,
         tokenId: tokenIdInput,
-      })
-/*
+      });
+      /*
       const preparedExecution = await tokenboundClient.prepareExecution({
         account: tokenboundAccount,
         to: recipientAddress,
@@ -144,14 +146,16 @@ export function App() {
         data: '',
       })
 */
-      const preparedCreateAccount = await tokenboundClient.prepareCreateAccount({
-        tokenContract: tokenContractInput as `0x${string}`,
-        tokenId: tokenIdInput,
-      })
+      const preparedCreateAccount = await tokenboundClient.prepareCreateAccount(
+        {
+          tokenContract: tokenContractInput as `0x${string}`,
+          tokenId: tokenIdInput,
+        }
+      );
 
-      console.log('getAccount', tokenboundAccount)
-//    console.log('preparedExecution', preparedExecution)
-      console.log('preparedAccount', preparedCreateAccount)
+      console.log("getAccount", tokenboundAccount);
+      //    console.log('preparedExecution', preparedExecution)
+      console.log("preparedAccount", preparedCreateAccount);
 
       // if (address) {
       //   walletClient?.sendTransaction(preparedCreateAccount)
@@ -159,20 +163,26 @@ export function App() {
       // }
     }
 
-    testTokenboundClass()
-    fetchNFTs()
-  }, [tokenContractInput, tokenIdInput])
+    testTokenboundClass();
+  }, [tokenContractInput, tokenIdInput]);
+
+  useEffect(() => {
+    async function wrapperFetchNFTs() {
+      setCurrentNFTs(await fetchNFTs(0));
+    }
+    wrapperFetchNFTs();
+  }, []);
 
   const createAccount = useCallback(async () => {
-    if (!tokenboundClient || !address) return
+    if (!tokenboundClient || !address) return;
     const createdAccount = await tokenboundClient.createAccount({
       tokenContract: tokenContractInput as `0x${string}`,
       tokenId: tokenIdInput,
-    })
-    console.log(`new account: ${createdAccount}`)
-    alert(`new account: ${createdAccount}`)
-  }, [tokenboundClient])
-/*
+    });
+    console.log(`new account: ${createdAccount}`);
+    alert(`new account: ${createdAccount}`);
+  }, [tokenboundClient]);
+  /*
   const execute = useCallback(async () => {
     if (!tokenboundClient || !address) return
     const executedCall = await tokenboundClient.execute({
@@ -195,156 +205,213 @@ export function App() {
   }, [tokenboundClient])
 */
 
-const Pinata_JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI2MmEzM2I0ZC1jZDYzLTQ1NmEtOWRiYi1hYmFhMGZmMWVkZTMiLCJlbWFpbCI6Imdvb2dvbGhlcm9pY3F1ZXN0c0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX0seyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiYzhlYmI1YjQ0OGMxM2M1Njc2MjYiLCJzY29wZWRLZXlTZWNyZXQiOiJjZTk1NjhjMzNkMWY5NTk4OGNiZmQzOWNhNGMxZGFiMGI0ZmU3N2UwMDY0Yzk2ZDQ2YzI2OGM4N2Q3MzBhZjZjIiwiaWF0IjoxNzE2NTI4NDc1fQ.8ogGbOwOAGhfP0lh9fwSn8ikBYw2Bo7sIdy-ZNuZmd8';
-const Pinata_Gateway = 'scarlet-capitalist-goat-720.mypinata.cloud';
+  const Pinata_JWT =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI2MmEzM2I0ZC1jZDYzLTQ1NmEtOWRiYi1hYmFhMGZmMWVkZTMiLCJlbWFpbCI6Imdvb2dvbGhlcm9pY3F1ZXN0c0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX0seyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiYzhlYmI1YjQ0OGMxM2M1Njc2MjYiLCJzY29wZWRLZXlTZWNyZXQiOiJjZTk1NjhjMzNkMWY5NTk4OGNiZmQzOWNhNGMxZGFiMGI0ZmU3N2UwMDY0Yzk2ZDQ2YzI2OGM4N2Q3MzBhZjZjIiwiaWF0IjoxNzE2NTI4NDc1fQ.8ogGbOwOAGhfP0lh9fwSn8ikBYw2Bo7sIdy-ZNuZmd8";
+  const Pinata_Gateway = "scarlet-capitalist-goat-720.mypinata.cloud";
 
-const imageToNFT = async (e: any) => {
+  const imageToNFT = async (e: any) => {
+    console.log(e.target);
 
-  console.log(e.target);
+    const formData = new FormData();
+    formData.append("file", e.target.files[0]);
 
-  const formData = new FormData();
-  formData.append("file", e.target.files[0]);
+    const metadata = JSON.stringify({
+      name: "Test images",
+    });
+    formData.append("pinataMetadata", metadata);
 
-  const metadata = JSON.stringify({
-    name: "Test images",
-  });
-  formData.append("pinataMetadata", metadata);
+    const options = JSON.stringify({
+      cidVersion: 0,
+    });
+    formData.append("pinataOptions", options);
 
-  const options = JSON.stringify({
-    cidVersion: 0,
-  });
-  formData.append("pinataOptions", options);
-
-  const res = await fetch(
-    "https://api.pinata.cloud/pinning/pinFileToIPFS",
-    {
+    const res = await fetch("https://api.pinata.cloud/pinning/pinFileToIPFS", {
       method: "POST",
       headers: {
         Authorization: `Bearer ${Pinata_JWT}`,
       },
       body: formData,
-    }
-  );
-  const resData = await res.json();
-  const imageURI = `https://${Pinata_Gateway}/ipfs/${resData.IpfsHash}` as string
-  console.log(imageURI);
+    });
+    const resData = await res.json();
+    const imageURI =
+      `https://${Pinata_Gateway}/ipfs/${resData.IpfsHash}` as string;
+    console.log(imageURI);
 
-  const name = 'sample' as string;
-  const [account] = await walletClient.getAddresses();
-  const contractAddress = '0x1255A9Fa337aE9E990Cb12B1fEa9a3D4EfC6bB73';
+    const name = "sample" as string;
+    const [account] = await walletClient.getAddresses();
+    const contractAddress = "0x1255A9Fa337aE9E990Cb12B1fEa9a3D4EfC6bB73";
 
-const { result } = await publicClient.simulateContract({
-  address: contractAddress,
-  abi: Web3Mint.abi,
-  functionName: 'mintIpfsNFT',
-  args: [name, resData.IpfsHash],
-  account: account,
-  chain: sepolia
-});
+    const { result } = await publicClient.simulateContract({
+      address: contractAddress,
+      abi: Web3Mint.abi,
+      functionName: "mintIpfsNFT",
+      args: [name, resData.IpfsHash],
+      account: account,
+      chain: sepolia,
+    });
 
+    const TxnHash = await walletClient.writeContract({
+      address: contractAddress,
+      abi: Web3Mint.abi,
+      functionName: "mintIpfsNFT",
+      args: [name, resData.IpfsHash],
+      account: account,
+      chain: sepolia,
+    });
+    console.log(`https://sepolia.etherscan.io/tx/${TxnHash}`);
 
-const TxnHash = await walletClient.writeContract({
-  address: contractAddress,
-  abi: Web3Mint.abi,
-  functionName: 'mintIpfsNFT',
-  args: [name, resData.IpfsHash],
-  account: account,
-  chain: sepolia
-});
-console.log(`https://sepolia.etherscan.io/tx/${TxnHash}`);
+    setTokenContractInput(contractAddress);
+    setTokenIdInput(`${Number(result)}`);
 
-setTokenContractInput(contractAddress);
-setTokenIdInput(`${Number(result)}`);
+    const unwatch = publicClient.watchEvent({
+      address: contractAddress,
+      onLogs: async (logs) => {
+        console.log("Started watching events.");
+        console.log(CurrentNFTs);
+        const newNFTs = await fetchNFTs(0);
+        setCurrentNFTs(newNFTs);
+        console.log(newNFTs);
+      },
+    });
 
-}
+    setTimeout(() => {
+      unwatch();
+      console.log("Stopped watching events.");
+    }, 60000);
 
-const Alchemy_API = "IkLCDxEjkwe4tb30m9XNxMu-HRmpeee0";
+  };
+
+  const Alchemy_API = "IkLCDxEjkwe4tb30m9XNxMu-HRmpeee0";
   const endpoint = `https://eth-sepolia.g.alchemy.com/v2/${Alchemy_API}`;
 
-  const fetchNFTs = async (retryAttempt = 0) => {
+  const sleep = (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  const fetchNFTs = async (second: number, retryAttempt = 0) => {
     const [account] = await walletClient.getAddresses();
     if (retryAttempt === 5) {
-        return;
+      return;
     }
     if (account) {
-        try {
-            const data = await fetch(`${endpoint}/getNFTs?owner=${account}`).then(data => data.json()) as OwnedNFTsResponse;
-            console.log(data.ownedNfts[0].contractMetadata.deployedBlockNumber);
-            setNFTs(data.ownedNfts.sort((a, b) => {
-              if (b.contractMetadata.deployedBlockNumber !== a.contractMetadata.deployedBlockNumber) 
-              {  return b.contractMetadata.deployedBlockNumber - a.contractMetadata.deployedBlockNumber; }
-              else
-              {  return Number(b.id.tokenId) - Number(a.id.tokenId); }
-            })
+      try {
+        await sleep(second * 1000);
+        const data = (await fetch(`${endpoint}/getNFTs?owner=${account}`).then(
+          (data) => data.json()
+        )) as OwnedNFTsResponse;
+        // console.log(data.ownedNfts[0].contractMetadata.deployedBlockNumber);
+        return await data.ownedNfts.sort((a, b) => {
+          if (
+            b.contractMetadata.deployedBlockNumber !==
+            a.contractMetadata.deployedBlockNumber
+          ) {
+            return (
+              b.contractMetadata.deployedBlockNumber -
+              a.contractMetadata.deployedBlockNumber
             );
-        } catch (e) {
-            fetchNFTs(retryAttempt+1);
-        }
+          } else {
+            return Number(b.id.tokenId) - Number(a.id.tokenId);
+          }
+        });
+      } catch (e) {
+        fetchNFTs(retryAttempt + 1);
+      }
     }
-  }
+  };
 
   return (
     <>
       <div className="title">
-      <h1>Web3概論 簡単デモ</h1>
-      <ConnectButton />
-      {isConnected && <Account />}
+        <h1>Web3概論 簡単デモ</h1>
+        <ConnectButton />
+        {isConnected && <Account />}
       </div>
-      {address && (<div style={{display: 'flex'}}>
-      <div style={{width: '50%'}}>
-      <h2>NFTアップローダー</h2>
-      <p>画像を選択すると、ERC-721のNFTをミントできます。</p>
-      <Button variant="contained">
-        画像を選択しＮＦＴをミントする
-        <input type="file" style={{
-          opacity: '0',
-          position: 'absolute',
-          top: '0',
-          left: '0',
-          width: '100%',
-          height: '100%',
-          cursor: 'pointer'
-        }} accept=".jpg , .jpeg , .png" onChange={imageToNFT} />
-      </Button>
-      
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '8px',
-            maxWidth: '480px',
-          }}
-        >
-        <h2>TokenBoundAccount生成機能</h2>
-        Token Contract(ERC-721)<input
-            type="text"
-            placeholder="Please input Token Contract here"
-            value={tokenContractInput}
-            onChange={(e) => setTokenContractInput(e.target.value)}
-        />
-        Token ID<input
-            type="text"
-            placeholder="Please input Token ID here"   
-            value={tokenIdInput}
-            onChange={(e) => setTokenIdInput(e.target.value)}
-        />
-        <Button variant="contained" onClick={() => createAccount()}>アカウントを生成する</Button>
-        </div>
+      {address && (
+        <div style={{ display: "flex" }}>
+          <div style={{ width: "50%" }}>
+            <h2>NFTアップローダー</h2>
+            <p>画像を選択すると、ERC-721のNFTをミントできます。</p>
+            <Button variant="contained">
+              画像を選択しＮＦＴをミントする
+              <input
+                type="file"
+                style={{
+                  opacity: "0",
+                  position: "absolute",
+                  top: "0",
+                  left: "0",
+                  width: "100%",
+                  height: "100%",
+                  cursor: "pointer",
+                }}
+                accept=".jpg , .jpeg , .png"
+                onChange={imageToNFT}
+              />
+            </Button>
 
-      </div>
-      <div style={{width: '50%'}}>
-        <h2>保有NFT(Sepolia Testnet)表示機能</h2>
-          <section style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-                {
-                    NFTs ? NFTs.slice(0,3).map(NFT => {
-                        return (
-                           <NftCard image={NFT.media[0].gateway} id={NFT.id.tokenId } title={NFT.title} address={NFT.contract.address} description={NFT.description}></NftCard>
-                        )
-                    }) : <div>No NFTs found</div>
-                }
-          </section>
-      </div>
-      </div>)}
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "8px",
+                maxWidth: "400px",
+              }}
+            >
+              <h2>TokenBoundAccount生成機能</h2>
+              Token Contract(ERC-721)
+              <input
+                type="text"
+                placeholder="Please input Token Contract here"
+                value={tokenContractInput}
+                onChange={(e) => setTokenContractInput(e.target.value)}
+              />
+              Token ID
+              <input
+                type="text"
+                placeholder="Please input Token ID here"
+                value={tokenIdInput}
+                onChange={(e) => setTokenIdInput(e.target.value)}
+              />
+              <Button variant="contained" onClick={() => createAccount()}>
+                アカウントを生成する
+              </Button>
+            </div>
+          </div>
+          <div style={{ width: "50%" }}>
+            <h2>保有NFT(Sepolia Testnet)表示機能</h2>
+            <NftList passedNFTs={CurrentNFTs} />
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
+
+interface NftListProp {
+  passedNFTs: NFT[] | undefined;
+}
+
+const NftList = ({ passedNFTs }: NftListProp) => {
+  return (
+    <section
+      style={{ display: "flex", flexWrap: "wrap", justifyContent: "left" }}
+    >
+      {passedNFTs ? (
+        passedNFTs.slice(0, 3).map((NFT) => {
+          return (
+            <NftCard
+              key={`${NFT.contract.address}_${NFT.id.tokenId}`}
+              image={NFT.media[0].gateway}
+              id={NFT.id.tokenId}
+              title={NFT.title}
+              address={NFT.contract.address}
+              description={NFT.description}
+            />
+          );
+        })
+      ) : (
+        <div>No NFTs found</div>
+      )}
+    </section>
+  );
+};
